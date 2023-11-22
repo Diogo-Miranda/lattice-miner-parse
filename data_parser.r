290c94ffb51ef9fabf3c12fd14972c4780055830
Diogo Araujo <57761628+Diogo-Miranda@users.noreply.github.com>
2023-11-21 18:40:04 -0800

    Update data_parser.r

--- data_parser.r
+++ data_parser.r
@@ -1,4 +1,21 @@
 library(foreign)
-path <- "C:\\Users\\diogo\\Desktop\\dava.sav"
-data <- foreign::read.spss(path, to.data.frame = TRUE)
-head(data)
+
+path <- "C:\\Users\\diogo\\Desktop\\data2.sav"
+data <- foreign:read.spss(path, to.data.frame = TRUE)
+
+sd <- subset(data, select = c(seq(7, 79)))
+sd <- subset(sd, select = -c(SOMS54_RECOD_2017))
+print(nrow(sd))
+
+d_zero <- sd[apply(sd, 1, function(row) all(row != 0)), ]
+d <- na.omit(d_zero)
+df <- data.frame(d)
+
+for (c in colnames(df)) {
+  df[[c]] <- ifelse(df[[c]] %in% c(1, 2,3), 1, 0)
+}
+print(nrow(df))
+df <- as.data.frame(df)
+head(df)
+
+write.csv(df, file = "C:\\Users\\diogo\\Desktop\\data2.csv")
